0e5659b867624d462f387db72d278ba6ce4090c8
unknown <lionel094@revature.net>
2022-08-26 17:01:15 -0700

    mergedList code revision

--- 20220822/mergedList/main.java
+++ 20220822/mergedList/main.java
@@ -40,6 +40,7 @@ lists[i] is sorted in ascending order.
 The sum of lists[i].length will not exceed 104.
 */
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Arrays;
 public class Main
 {
@@ -52,20 +53,20 @@ public class Main
         lists[2] = new LinkedList<Integer>(Arrays.asList(2,6));
 */        
         int k = Math.round(Math.round(Math.random() * 104)); //amount of LinkedLists
-        LinkedList<Integer>[] lists = new LinkedList[k];
+        List<Integer>[] lists = new LinkedList[k];
         for(int i = 0; i < k; i++){
             int elements = Math.round(Math.round(Math.random() * 500)); //amount of elements per LinkedList
             lists[i] = new LinkedList<Integer>();
             for(int j = 0; j < elements; j++)
-                lists[i].push(Math.round(Math.round(Math.random() * 208) - 104)); //possible values of elements
+                lists[i].add(Math.round(Math.round(Math.random() * 208) - 104)); //possible values of elements
             Integer[] sortMe = lists[i].toArray(new Integer[lists[i].size()]);
             Arrays.sort(sortMe);
             lists[i] = new LinkedList<Integer>(Arrays.asList(sortMe));
         }
         
         
-        LinkedList<Integer> mergedList = new LinkedList<Integer>();
-        for(LinkedList l:lists){
+        List<Integer> mergedList = new LinkedList<Integer>();
+        for(List l:lists){
             mergedList.addAll(l);
         }
         Integer[] mergedListSort = mergedList.toArray(new Integer[mergedList.size()]);
